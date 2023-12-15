0348481ceac99c88063649534d76da2e77d86179
Houssine Bakkali <houssine.bakkali@gmail.com>
2023-12-15 18:16:26 +0100

    [ADD] add base custom module

---
+++ custom-addons/propage_base/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Open Architects Consulting SRL (https://www.openarchitecsconsulting.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Propage-s Base Module",
+    "version": "16.0.1.0.0",
+    "category": "CRM",
+    "author": "Open Architects Consulting",
+    "website": "https://www.openarchitecsconsulting.com",
+    "license": "AGPL-3",
+    "depends": [
+        "crm",
+        "project",
+        "propage_community_base"
+        
+    ],
+    "installable": True,
+}
