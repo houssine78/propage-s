f30dda1f6c3aeae30870dc5cf5aeef258011955f
Houssine Bakkali <houssine.bakkali@gmail.com>
2023-12-15 18:16:06 +0100

    [ADD] add community module

---
+++ community-addons/propage_community_base/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Open Architects Consulting SRL (https://www.openarchitecsconsulting.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Propage-s Community Base Module",
+    "version": "16.0.1.0.0",
+    "category": "CRM",
+    "author": "Open Architects Consulting",
+    "website": "https://www.openarchitecsconsulting.com",
+    "license": "AGPL-3",
+    "depends": [
+        "disable_odoo_online",
+        "remove_odoo_enterprise",
+        "web_responsive",
+        "web_refresher",
+        "web_widget_x2many_2d_matrix",
+        "partner_firstname",
+        "partner_contact_birthdate",
+        "partner_contact_gender",
+        "partner_multi_relation",
+        "crm_lead_firstname",
+        "dms",
+        "hr_timesheet_sheet",
+        "hr_employee_firstname",
+        "project_task_link",
+        "project_template"
+    ],
+    "installable": True,
+}
