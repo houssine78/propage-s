f8c8d008b73a7023b15646be1bc80dd05bbd12b6
Houssine Bakkali <houssine.bakkali@gmail.com>
2024-01-21 14:50:46 +0100

    [ADD] add propage_profile module

---
+++ custom-addons/propage_profile/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Open Architects Consulting SRL (https://www.openarchitecsconsulting.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Propage-s Custom Profile Module",
+    "description": "This module install all the required"
+    " custom module for the propage project",
+    "version": "16.0.1.0.0",
+    "category": "Base",
+    "author": "Open Architects Consulting",
+    "website": "https://www.openarchitecsconsulting.com",
+    "license": "AGPL-3",
+    "depends": [
+        "propage_base",
+        "propage_crm",
+        "propage_project",
+        "propage_entrepreneur_case"
+    ],
+    "installable": True,
+}
