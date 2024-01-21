d716423823ef1542a691bb44ef298e0bbdfee78f
Houssine Bakkali <houssine.bakkali@gmail.com>
2024-01-21 14:49:43 +0100

    [ADD] add propage_project module

---
+++ custom-addons/propage_project/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Open Architects Consulting SRL (https://www.openarchitecsconsulting.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Propage-s Custom Project Module",
+    "version": "16.0.1.0.0",
+    "category": "Project",
+    "author": "Open Architects Consulting",
+    "website": "https://www.openarchitecsconsulting.com",
+    "license": "AGPL-3",
+    "depends": [
+        "propage_base",
+        "project"
+    ],
+    "installable": True,
+}
