6beb57752868c32a174459f1a316ba50e961ddbf
Houssine Bakkali <houssine.bakkali@gmail.com>
2026-02-06 17:07:50 +0100

    [IMP] calendar_warning field to warn if not enough hours are logged

--- custom-addons/propage_project/models/account_analytic_line.py
+++ custom-addons/propage_project/models/account_analytic_line.py
@@ -14,3 +14,24 @@ class AccountAnalyticLine(models.Model):
     is_fse = fields.Boolean(related="task_id.is_fse")
     customer_id = fields.Many2one("res.partner")
     calendar_event_id = fields.Many2one("calendar.event")
+    calendar_warning = fields.Boolean(
+        compute="_compute_calendar_warning",
+        help="Technical computed field to warn when not enough time is "
+             "registered for the calendar event timesheet"
+    )
+
+    def _compute_calendar_warning(self):
+        AnalLine = self.env['account.analytic.line'].sudo()
+
+        for line in self:
+            event_id = line.calendar_event_id
+            calendar_warning = False
+            if event_id:
+                duration = event_id.duration
+                lines = line.task_id.timesheet_ids.filtered(
+                            lambda l: l.calendar_event_id.id == event_id.id and
+                                      l.employee_id.id == line.employee_id.id
+                )
+                if sum(lines.mapped('unit_amount')) < duration:
+                    calendar_warning = True
+                line.calendar_warning = calendar_warning
